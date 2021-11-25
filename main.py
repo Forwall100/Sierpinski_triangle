35d0a0e77a7083502f8762253241ee5e0793c1e6
Forwall100 <78537089+Forwall100@users.noreply.github.com>
2021-11-26 00:20:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Example(Frame):
         self.initUI()
  
     def initUI(self):
-        self.master.title("Треугольник серпинского")
+        self.master.title("Треугольник Серпинского")
         self.pack(fill=BOTH, expand=1)
  
         canvas = Canvas(self)
